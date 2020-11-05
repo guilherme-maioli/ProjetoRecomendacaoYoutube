547ae1044ad5b27104e4a6e9fd5204574786e162
guimaioli <guitue@gmail.com>
2020-11-05 10:42:58 -0300

    version 3

--- clean_deploy/app.py
+++ clean_deploy/app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template
 import os
 import json
 import run_backend
-import youtube_dlc
 import get_data
 import ml_utils
 
@@ -33,42 +32,54 @@ def get_predictions():
 	# do maior pro menor do ultimo elemento = score
 	predictions = sorted(predictions, key=lambda x: x[2], reverse=True)[:30]
 
-	#predictions_formatted = []
-	#for e in predictions:
-	#	#print(e)
-	#	predictions_formatted.append("<tr><th><a href=\"{link}\">{title}</a></th><th>{score}</th></tr>".format(title=e[1], link=e[0], score=e[2]))
-
-	#return '\n'.join(predictions_formatted)
 	return predictions
 
 @app.route('/')
 def main_page():
 	preds = get_predictions()
-	#print(preds)
-	#return """ <head><h1>Recomendador de Vídeos do Youtube</h1></head>
-	#<body>
-	#<table>
-	#		{}
-	#</table>
-	#</body>""".format(preds)
 
 	return render_template('index.html', posts=preds)
 
 
-@app.route('/predict')
-def predict_api():
-	yt_videos_id = request.args.get("yt_videos_id", default='')
+def predict_api(id_video):
 	
 	ydl = youtube_dlc.YoutubeDL({"ignoreerrors": True})
-	video_json_data = ydl.extract_info("https://www.youtube.com/watch?v={}".format(yt_videos_id), download=False)
-
+	video_json_data = ydl.extract_info("https://www.youtube.com/watch?v={}".format(id_video), download=False)
 
 	if video_json_data is None:
 		return "not found"
 
 	p = ml_utils.compute_prediction(video_json_data)
-	output = {"title":video_json_data['title'], "score":p}
-	return json.dumps(output)
+	output = {"title":video_json_data['title'], "score":p, "link":"https://www.youtube.com/watch?v={}".format(id_video) }
+
+	return output
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+	if request.method == 'GET':
+
+		return render_template('prediction.html')
+	
+	elif request.method == 'POST':
+
+		id_video = request.form["video"]
+		id_video = id_video.split("v=")
+		videos = predict_api(id_video[1])
+
+		# caso api não encontre o video.
+		if videos == "not found":
+			return json.dumps(videos)
+
+		print("\n")
+		print(videos)
+		print("\n")
+
+		link = "https://www.youtube.com/embed/"+id_video[1]
+		print(videos["title"])
+		print(videos["score"])
+		print(link)
+
+		return render_template('result.html', title=videos["title"], score=videos["score"], link=link)
 
 
 
